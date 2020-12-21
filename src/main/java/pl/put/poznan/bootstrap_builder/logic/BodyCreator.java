55f365f2dc0bc1ba1fa5c77e1547e8ef4889689a
pGumowski38 <p.gumowski38@gmail.com>
2020-12-21 18:37:18 +0100

    Remove BodyCreator, HeadCreator, HtmlCreator

--- src/main/java/pl/put/poznan/bootstrap_builder/logic/BodyCreator.java
+++
@@ -1,7 +0,0 @@
-package pl.put.poznan.bootstrap_builder.logic;
-
-public class BodyCreator extends HtmlCreator{
-    public BodyCreator() {
-        setComponent(new Body());
-    }
-}
--- src/main/java/pl/put/poznan/bootstrap_builder/logic/HeadCreator.java
+++
@@ -1,7 +0,0 @@
-package pl.put.poznan.bootstrap_builder.logic;
-
-public class HeadCreator extends HtmlCreator{
-    public HeadCreator() {
-        this.setComponent(new Head());
-    }
-}
