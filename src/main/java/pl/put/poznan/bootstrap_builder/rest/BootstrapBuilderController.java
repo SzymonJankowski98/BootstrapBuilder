9623659d1b1081d7bff9bdd897af437ad74a4836
Szymon <jankowski.szymon.98@gmail.com>
2020-12-21 19:54:40 +0100

    Update pom, BootstrapBuilderController

--- src/main/java/pl/put/poznan/bootstrap_builder/rest/BootstrapBuilderController.java
+++ src/main/java/pl/put/poznan/bootstrap_builder/rest/BootstrapBuilderController.java
@@ -4,6 +4,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.bootstrap_builder.logic.BootstrapBuilder;
 
+import java.util.Date;
+
 @RestController
 public class BootstrapBuilderController {
 
@@ -13,6 +15,11 @@ public class BootstrapBuilderController {
     public String test(@RequestParam(value="nav", defaultValue="none") String nav,
                        @RequestParam(value="footer", defaultValue="false") String foot) {
         BootstrapBuilder bootstrapBuilder = new BootstrapBuilder(nav, foot);
+
+        Date date = new Date();
+        logger.info(date.toString() + "User connected to server");
+        logger.debug(date.toString() + ": nav=" + nav + " footer=" + foot);
+
         return bootstrapBuilder.create();
     }
 }
